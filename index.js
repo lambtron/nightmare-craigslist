02e3b19b72621f4fc2e7ad37aafb2be36e985345
Andy Jiang <andyjiang@gmail.com>
2014-11-30 01:24:47 -0800

    added login, setlocation

--- index.js
+++ index.js
@@ -8,6 +8,22 @@ var request = require('superagent');
 var fs = require('fs');
 
 /**
+ * Set location of Craigslist.
+ *
+ * @param {String} location
+ */
+
+var setLocation = exports.setLocation = function(location) {
+  return function(nightmare) {
+    nightmare
+      .viewport(800, 1600)
+      .goto('http://www.craigslist.org/about/sites')
+      .click('a[href*="' + location + '"]')
+      .wait(1000);
+  };
+};
+
+/**
  * Login to a Craigslist account.
  *
  * @param {String} email
@@ -16,28 +32,58 @@ var fs = require('fs');
 
 var login = exports.login = function(email, password){
   return function(nightmare) {
-    // WIP
+    nightmare
+      .viewport(800, 1600)
+      .goto('https://accounts.craigslist.org/login')
+      .type('#inputEmailHandle', email)
+      .type('#inputPassword', password)
+      .click('p > button[type="submit"]')
+      .wait(1000);
   };
 };
 
 /**
  * Perform a search.
  *
+ * @param {String} type
  * @param {String} query
  */
 
 var search = exports.search = function(type, query) {
   return function(nightmare) {
+    nightmare
+      .exists('#query', function(ready) {
+        if (!ready) throw 'Search box not ready.';
+        return;
+      })
+      .type('#query', query)
+      .evaluate(function() {
+        // TODO: need to set the type.
+      }, function() {
+      })
+      .click('#go')
+      .wait(1000);
+  };
+};
+
+/**
+ * Get listing.
+ */
+
+var getListing = exports.getListing = function() {
+  return function(nightmare) {
     // WIP
-  }
+  };
 };
 
 /**
  * Create listing.
+ *
+ * @param {Object} listing
  */
 
-var createListing = exports.createListing = function() {
+var createListing = exports.createListing = function(listing) {
   return function(nightmare) {
     // WIP
-  }
+  };
 };
